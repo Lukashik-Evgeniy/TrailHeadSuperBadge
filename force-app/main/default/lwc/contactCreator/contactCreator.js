eb111eabb7306dde6553deedd0b7bf59d85b447e
Lukashik-Evgeniy <eugenelukashikwork@gmail.com>
2023-02-24 14:03:38 +0300

    added flex page + tab + app. updated script

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,7 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-import CONTACT_OBJECT from '@salesforce/schema/Contact';
 
 import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
@@ -9,25 +7,8 @@ import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 
 export default class ContactCreator extends LightningElement {
     fields = [FIRST_NAME_FIELD, LAST_NAME_FIELD, EMAIL_FIELD];
-    objectApiName = CONTACT_OBJECT;
-
-    handleSuccess(event) {
-        const recordId = event && event.detail && event.detail.id || '';
-
-        if (recordId) {            
-            const toastEvent = new ShowToastEvent({
-                title: "Contact created",
-                message: "Record ID: " + recordId,
-                variant: "success"
-            });
-            
-            this.dispatchEvent(toastEvent);
-        }
-    }
-
-}
-
     @api objectApiName = 'Contact';
+
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: 'Contact created',
---
+++ new-scratch-deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+PACKAGE_ID=04t6g000008ateoAAA
+
+while [ ! -n "$ORG_NAME"  ] 
+do
+	echo "🐱  Please enter a name for your scratch org:"
+	read ORG_NAME
+done
+
+echo "🐱  Building your org, please wait."
+sfdx force:org:create -w 10 -f config/project-scratch-def.json --setalias ${ORG_NAME} --durationdays 7 --setdefaultusername --json --loglevel DEBUG
+
+if [ "$?" = "1" ] 
+then
+	echo "🐱 Can't create your org."
+	exit
+fi 
+
+echo "🐱 Scratch org created."
+echo "🐱 Installing package with ID ${PACKAGE_ID}, please wait"
+
+RES=`sfdx force:package:install --package ${PACKAGE_ID} -u ${ORG_NAME} --json -w 10`
+
+if [ "$?" = "1" ] 
+then
+	echo "🐱  Can't install this package."
+	exit
+fi
+
+INSTALL_ID=`echo ${RES} | egrep -o '"Id":"(.*?)"' | cut -d : -f 2 | tr -d '"'`
+echo "🐱 Package install in progress, please wait."
+
+STATUS=init
+while [ $STATUS != 'SUCCESS' ]
+do
+	RES=`sfdx force:package:install:get -i ${INSTALL_ID} -u ${ORG_NAME} --json`
+	STATUS=`echo ${RES} | egrep -o '"Status":".*?"' | cut -d : -f 2 | tr -d '"' `
+	echo "🍺  Install status is ${STATUS}. Please wait."
+	sleep 10
+done
+
+echo "🐱  Pushing the code, please wait. It may take a while."
+
+
+echo "🐱 Scratch org created."
+
+sfdx force:source:push -u ${ORG_NAME}
+
+if [ "$?" = "1" ]
+then 
+	echo "🐱  Can't push your source."
+	exit 
+fi
+
+echo "🐱  Code is pushed successfully."
+
+sfdx force:org:open -u ${ORG_NAME}
